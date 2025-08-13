fb5a57e998ab486196e23f7cefdf3df9e708b3a4
Durgesh Yadav <165238289+durgeshyadavcse@users.noreply.github.com>
2025-08-13 18:39:24 +0530

    Update messageModel.js

--- backend/models/messageModel.js
+++ backend/models/messageModel.js
@@ -1,19 +1,20 @@
 import mongoose from "mongoose";
 
-const messageModel = new mongoose.Schema({
-    senderId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
-        required:true
-    },
-    receiverId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
-        required:true
-    },
-    message:{
-        type:String,
-        required:true
-    }
-},{timestamps:true});
-export const Message = mongoose.model("Message", messageModel); 
+const messageSchema = new mongoose.Schema({
+  conversationId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Conversation",
+    required: true
+  },
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true });
+
+export const Message = mongoose.model("Message", messageSchema);
