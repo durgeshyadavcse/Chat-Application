61aa090443a49d633a2c88fdc0944bfba454fa98
Durgesh Yadav <165238289+durgeshyadavcse@users.noreply.github.com>
2025-08-13 18:05:55 +0530

    Update isAuthenticated.js

--- backend/middleware/isAuthenticated.js
+++ backend/middleware/isAuthenticated.js
@@ -1,23 +1,27 @@
 import jwt from "jsonwebtoken";
-const isAuthenticated = async(req,res,next) => {
+
+const isAuthenticated = async (req, res, next) => {
   try {
     const token = req.cookies.token;
-    if(!token){
-        return res.status(401).json({message:"User not authenticated."})
-    };
-    const decode = await jwt.verify(token,process.env.JWT_SECRET_KEY);
-    if(!decode){
-        return res.status(401).json({message:"Invalid token"});
-    };
-    req.id = decode.userId;
-    next();
+
+    if (!token) {
+      return res.status(401).json({ message: "User not authenticated." });
+    }
+
+    const decoded = await jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    if (!decoded) {
+      return res.status(401).json({ message: "Invalid token." });
+    }
+
+    // Set the authenticated user's ID in the request object
+    req.id = decoded.userId;
+
+    next(); // Move to the next middleware/route
   } catch (error) {
     console.log(error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
-export default isAuthenticated;
 
-const req = {
-    id:"",
-}
-req.id = "sdlbgnjdfn"
+export default isAuthenticated;
